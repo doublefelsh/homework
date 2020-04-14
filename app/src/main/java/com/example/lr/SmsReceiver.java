d13a1bdae97615c428e407ff65dc4e420c8aa4a5
doubleflesh <2662466952@qq.com>
2020-04-14 16:49:14 +0800

    first test

--- app/src/main/java/com/example/lr/SmsReceiver.java
+++ app/src/main/java/com/example/lr/SmsReceiver.java
@@ -12,12 +12,12 @@ public class SmsReceiver extends BroadcastReceiver {
         //在广播组件里，通过上下文对象启动音乐播放服务组件
         context.startService(serviceIntent);
 
-        //新建调用Activity组件的意图
-        Intent activityIntent = new Intent(context, MainActivity.class);
+    //新建调用Activity组件的意图
+    Intent activityIntent = new Intent(context, MainActivity.class);
         activityIntent.putExtra("iscast", true);  //携带数据
-        //新建栈用来存放被启动的Activity（当已经存在时，只做移动处理）
-        activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        //在广播组件里，通过上下文对象启动Activity组件
-        context.startActivity(activityIntent);
-    }
-}
+                //新建栈用来存放被启动的Activity（当已经存在时，只做移动处理）
+                activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //在广播组件里，通过上下文对象启动Activity组件
+                context.startActivity(activityIntent);
+                }
+                }
