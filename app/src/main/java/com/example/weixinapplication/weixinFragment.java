3073645eeea376bd2d62d37e869187f208600995
doubleflesh <2662466952@qq.com>
2020-05-06 20:37:07 +0800

    第一次提交

---
+++ app/src/main/java/com/example/weixinapplication/weixinFragment.java
@@ -0,0 +1,97 @@
+package com.example.weixinapplication;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import android.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class weixinFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<String> mList = new ArrayList<>();
+    private Context context;
+    private adapter_swipe adapter;
+
+    public weixinFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.tab01, container, false);
+
+        recyclerView=view.findViewById(R.id.rcv_swipe);
+        initData();
+        initView();
+
+        return view;
+    }
+
+    private void initData() {
+        mList.add("亚特兰大老鹰");
+        mList.add("夏洛特黄蜂");
+        mList.add("迈阿密热火");
+        mList.add("奥兰多魔术");
+        mList.add("华盛顿奇才");
+        mList.add("波士顿凯尔特人");
+        mList.add("布鲁克林篮网");
+        mList.add("纽约尼克斯");
+        mList.add("费城76人");
+        mList.add("多伦多猛龙");
+        mList.add("芝加哥公牛");
+        mList.add("克里夫兰骑士");
+        mList.add("底特律活塞");
+        mList.add("印第安纳步行者");
+        mList.add("密尔沃基雄鹿");
+        mList.add("达拉斯独行侠");
+        mList.add("休斯顿火箭");
+        mList.add("孟菲斯灰熊");
+        mList.add("新奥尔良鹈鹕");
+        mList.add("圣安东尼奥马刺");
+        mList.add("丹佛掘金");
+        mList.add("明尼苏达森林狼");
+        mList.add("俄克拉荷马城雷霆");
+        mList.add("波特兰开拓者");
+        mList.add("犹他爵士");
+        mList.add("金州勇士");
+        mList.add("洛杉矶快船");
+        mList.add("洛杉矶湖人");
+        mList.add("菲尼克斯太阳");
+        mList.add("萨克拉门托国王");
+    }
+
+    private void initView(){
+        context=this.getActivity();
+        adapter=new adapter_swipe(context,mList);
+
+        ItemTouchHelper.Callback callback = new SwipeItemTouchHelper(adapter);
+        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+        touchHelper.attachToRecyclerView(recyclerView);
+
+        LinearLayoutManager manager=new LinearLayoutManager(context);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setHasFixedSize(true);
+    }
+
+}
